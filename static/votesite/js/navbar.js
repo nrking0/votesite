b0e3752977cbf11fbb9a00cfab90387e8469e7ec
Nick King <nrking0@gmail.com>
2020-08-10 13:54:08 -0500

    static

---
+++ static/votesite/js/navbar.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 600) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
