f16aea84e98368e5dab216b7d3ba879136bb6a66
Nick King <nrking0@gmail.com>
2020-08-09 21:29:50 -0500

    static

--- votesite/settings.py
+++ votesite/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'votesite/static/'
+#STATIC_URL = 'votesite/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'votesite/static')
 ]
