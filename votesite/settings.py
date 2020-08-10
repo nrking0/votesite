2bb658c948f5252f4a55b7a9bc72d63edaa79849
Nick King <nrking0@gmail.com>
2020-08-09 20:17:50 -0500

    update hosts

--- votesite/settings.py
+++ votesite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG") == 'True')
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['votesite.herokuapp.com']
 
 
 # Application definition
