64972836f06de49393b6307057b7818e1a478806
Nick King <nrking0@gmail.com>
2020-09-14 16:50:09 -0500

    Update profile fixed

--- votesite/forms.py
+++ votesite/forms.py
@@ -2,12 +2,11 @@ from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
-class editForm(UserChangeForm):
+class editForm(forms.ModelForm):
     username = forms.CharField(max_length=100, widget= forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=100, widget= forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, widget= forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget= forms.EmailInput(attrs={'class': 'form-control'}))
-    
 
     class Meta:
         model = User
